2fd33ff7cf93345fbfd14ce525e9c73f194ce8f4
Saem <ottosigma@gmail.com>
2017-02-17 02:58:46 +0200

    Initial commit

---
+++ ios-moe/src/com/sameperson/game/IOSMoeLauncher.java
@@ -0,0 +1,26 @@
+package com.sameperson.game;
+
+import com.badlogic.gdx.backends.iosmoe.IOSApplication;
+import com.badlogic.gdx.backends.iosmoe.IOSApplicationConfiguration;
+import org.moe.natj.general.Pointer;
+import com.sameperson.game.Application;
+
+import apple.uikit.c.UIKit;
+
+public class IOSMoeLauncher extends IOSApplication.Delegate {
+
+    protected IOSMoeLauncher(Pointer peer) {
+        super(peer);
+    }
+
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.useAccelerometer = false;
+        return new IOSApplication(new Application(), config);
+    }
+
+    public static void main(String[] argv) {
+        UIKit.UIApplicationMain(0, null, null, IOSMoeLauncher.class.getName());
+    }
+}
